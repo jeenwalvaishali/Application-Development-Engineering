163d4c6cea631d462464e23281fddd90836ddc11
Vaishali Jeenwal <jeenwal.v@northeastern.edu>
2022-02-22 11:17:48 -0800

    Committing quiz 1 in main branch

---
+++ Quiz1/Professor.java
@@ -0,0 +1,12 @@
+package Quiz1;
+
+public class Professor extends Person {
+
+    public Professor(String name, int id, int age) {
+        super(name, id, age);
+    }
+
+    public void giveQuiz(String quiz){
+        System.out.println(quiz + "Question of the quiz");
+    }
+}
