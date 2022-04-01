d4ae68e11f97d3aa9c44ed7fec9a3f83f705c804
Vaishali Jeenwal <jeenwal.v@northeastern.edu>
2022-03-31 17:59:47 -0700

    This commit does assignment 4 ThreadSafeQueue

--- Assignment 4/src/com/company/Main.java
+++ Assignment 4/src/com/company/Main.java
@@ -10,23 +10,66 @@ public class Main {
         /*Creating object of ThreadSafeQueue class*/
         ThreadSafeQueue threadSafeQueue = new ThreadSafeQueue<>();
 
-        /*Adds elements {0, 1, 2, 3, 4}*/
-        threadSafeQueue.add(1);
-        threadSafeQueue.add(2);
-        threadSafeQueue.add(3);
-        threadSafeQueue.add(4);
 
-        /* To view the head of queue */
-        System.out.println(threadSafeQueue.peek());
+        Thread thread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                for(int i=0; i<5; i++){
+                    try {
+                        /*Adds elements {0, 1, 2, 3, 4....}*/
+                        threadSafeQueue.add(i);
 
-        /* To remove elements from queue that is head of the queue*/
-        threadSafeQueue.remove();
+                        /* To view the head of queue */
+                        System.out.println(threadSafeQueue.peek());
 
-        /* To view the head of queue after removing head */
-        System.out.println(threadSafeQueue.peek());
+                        Thread.sleep(200);
 
-        /*Checking queue is empty or not*/
-        System.out.println(threadSafeQueue.isEmpty());
+                        /* To remove elements from queue that is head of the queue*/
+                        threadSafeQueue.remove();
+
+                        /*Checking queue is empty or not*/
+                        System.out.println(threadSafeQueue.isEmpty());
+                        
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                for(int j=10; j<15; j++){
+                    try {
+                        /*Adds elements {0, 1, 2, 3, 4....}*/
+                        threadSafeQueue.add(j);
+
+                        /* To view the head of queue */
+                        System.out.println(threadSafeQueue.peek());
+
+                        Thread.sleep(200);
+
+                        /* To remove elements from queue that is head of the queue*/
+                        threadSafeQueue.remove();
+
+                        /* To view the head of queue after removing head */
+                        System.out.println(threadSafeQueue.peek());
+
+                        /*Checking queue is empty or not*/
+                        System.out.println(threadSafeQueue.isEmpty());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread.start();
+        thread2.start();
+
 
     }
 
